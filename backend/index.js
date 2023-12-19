3fc36287c8f1a9091af334867e8ee5dc3bfeca21
Emanuele Pica <emanuelepica@Air-di-Emanuele.Home>
2023-12-19 12:09:30 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const AWS = require("aws-sdk");
+const parser = require("lambda-multipart-parser");
+const s3 = new AWS.S3();
+const rekognition = new AWS.Rekognition();
+
+
+async function saveFile(file) {
+  console.log({file})
+  const BucketName = process.env.BUCKET_NAME;
+  console.log(BucketName);
+  const savedFile = await s3
+    .putObject({
+        Bucket: BucketName,
+        Key: file.filename,
+        Body: file.content,
+    })
+    .promise();
+  const labels = await rekognition.detectLabels({
+    Image: {
+      Bytes: file.content,
+    }
+  }).promise();
+  return {savedFile, labels};
+}
+
+module.exports.savePhoto = async (event) => {
+  const { files } = await parser.parse(event);
+  const filesData = files.map(saveFile);
+  const results = await Promise.all(filesData);
+
+  return{
+    statusCode: 200,
+    body: JSON.stringify({
+      results,
+    })
+  }
+};
